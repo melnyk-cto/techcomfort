aba6e38f05ac08b3009b8deefc0814814eb233d9
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-10-17 22:37:01 +0300

    fix: "Наши работы"

--- wp-content/themes/techcomfort/assets/js/our-works.js
+++ wp-content/themes/techcomfort/assets/js/our-works.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const ourWorks = document.querySelector('.our-works');
     ourWorks.style.opacity = 1;
-    // Gallery "Наши работы" page
-    // const gallery = document.getElementsByClassName('gallery-item');
-    // for (let i = 0; i < gallery.length; i++) {
-    //     gallery[i].addEventListener('click', function () {
-    //         this.classList.add('fixed')
-    //     });
-    // }
+
+    // zoom on click
+    const gallery = document.getElementsByClassName('gallery-item');
+    for (let i = 0; i < gallery.length; i++) {
+        gallery[i].addEventListener('click', function () {
+            this.classList.add('fixed')
+        });
+    }
 });
--- wp-content/themes/techcomfort/pages/our-works.php
+++ wp-content/themes/techcomfort/pages/our-works.php
@@ -1,124 +1,26 @@
 <?php /* Template Name: Page - Our Works */ ?>
 <?php get_header(); ?>
 <?php
-wp_enqueue_script('salvatore-js', get_stylesheet_directory_uri() . '/assets/lib/salvatore.min.js');
-wp_enqueue_script('our-works-js', get_stylesheet_directory_uri() . '/assets/js/our-works.js');
+    wp_enqueue_script('salvatore-js', get_stylesheet_directory_uri() . '/assets/lib/salvatore.min.js');
+    wp_enqueue_script('our-works-js', get_stylesheet_directory_uri() . '/assets/js/our-works.js');
 ?>
-<main class='our-works'>
-    <section class='gallery'>
-        <div class='container'>
-            <h2>НАШИ РАБОТЫ</h2>
-            <div id='grid' data-columns class='clearfix'>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-1.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item grid-item--width2">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-2.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-3.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-4.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item grid-item--width2">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-5.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-6.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item grid-item--width2">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-7.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-8.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-9.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item grid-item--width2">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-10.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-11.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-12.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-13.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item grid-item--width2">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-14.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-15.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-16.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-17.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-18.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item grid-item--width2">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-19.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-20.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-21.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-22.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-23.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-24.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-25.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-26.jpg'
-                         alt='Кондиционер'>
-                </div>
-                <div class="gallery-item">
-                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/product-gallery-27.jpg'
-                         alt='Кондиционер'>
+    <main class='our-works'>
+        <section class='gallery'>
+            <div class='container'>
+                <h2>НАШИ РАБОТЫ</h2>
+                <div id='grid' data-columns class='clearfix'>
+                    <?php
+                        $images = get_field('our-works-gallery');
+                        if ($images): ?>
+                            <?php foreach ($images as $image): ?>
+                                <div class="gallery-item">
+                                    <img src='<?php echo esc_url($image['url']); ?>'
+                                         alt="<?php echo esc_attr($image['alt']); ?>">
+                                </div>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
                 </div>
             </div>
-        </div>
-    </section>
-</main>
+        </section>
+    </main>
 <?php get_footer(); ?>
