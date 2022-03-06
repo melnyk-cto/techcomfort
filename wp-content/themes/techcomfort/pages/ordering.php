c34098158bffb9c43fa245208c55a4580d8a6fc4
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2022-03-06 22:25:55 +0200

    fix: add ordering form

--- wp-content/themes/techcomfort/pages/ordering.php
+++ wp-content/themes/techcomfort/pages/ordering.php
@@ -2,7 +2,7 @@
 <?php get_header(); ?>
 <?php global $woocommerce; ?>
 <script>
-    const products = [];
+  const products = [];
 </script>
 <main class='ordering'>
     <section class='contact'>
@@ -10,7 +10,94 @@
             <h2>Оформление заказа</h2>
             <div class='contact-inner'>
                 <div class='contact-form'>
-                    <?php echo do_shortcode('[contact-form-7 id="10731" title="Оформление заказа"]') ?>
+                    <form action=''>
+                        <textarea class='products-js' name='textarea-products'></textarea>
+                        <label>
+                            <span class="label">Имя*</span>
+                            <input type='text' name='first-name' required>
+                        </label>
+                        <label>
+                            <span class="label">Фамилия*</span>
+                            <input type='text' name='last-name' required>
+                        </label>
+                        <label>
+                            <span class="label">Телефон*</span>
+                            <input type='tel' name='phone' required>
+                        </label>
+                        <label>
+                            <span class="label">Email*</span>
+                            <input type='email' name='email' required>
+                        </label>
+                        <label class='address'>
+                            <span class="label">Адрес</span>
+                            <textarea name='address'></textarea>
+                        </label>
+                        <div class='installation-required'>
+                            <h5>Требуется установка</h5>
+                            <label>
+                                <input required value="Да" name='installation' type="radio" checked>
+                                Да
+                            </label>
+                            <label>
+                                <input value="Нет" name='installation' type="radio">
+                                Нет
+                            </label>
+                            <label class='textarea'>
+                                <span class='label'>Дополнительная информация</span>
+                                <textarea name="Additional"></textarea>
+                            </label>
+                        </div>
+                        <div class='delivery-service'>
+                            <h5>Сервис доставки</h5>
+                            <div class='delivery-service-item'>
+                                <label class='courier'>
+                                    <input value="Курьер" name='delivery' type='radio'>
+                                    Курьер
+                                </label>
+                                <p>доставка по Киеву бесплатно</p>
+                            </div>
+                            <div class='delivery-service-item'>
+                                <label class='new-mail'>
+                                    <input value="Новая почта (по тарифу перевозчика)" name='delivery' type='radio'>
+                                    Новая почта ( по тарифу перевозчика )
+                                </label>
+                                <label>
+                                    <span class='label'>Адрес</span>
+                                    <input type='text' class='address-mail'>
+                                </label>
+                                <label>
+                                    <span class='label'>Отделение</span>
+                                    <input type='text' name='department-mail'>
+                                </label>
+                            </div>
+                            <div class='delivery-service-item'>
+                                <label>
+                                    <input value="Другой вариант доставки" name='delivery' type='radio' checked>
+                                    Другой вариант доставки
+                                </label>
+                            </div>
+                        </div>
+                        <div class='payment-method'>
+                            <h5>Способ оплаты</h5>
+                            <label>
+                                <input value="Оплата наличными" name='payment' type='radio'>
+                                Оплата наличными
+                            </label>
+                            <label>
+                                <input value="Оплата картой" name='payment' type='radio' checked>
+                                Оплата картой
+                            </label>
+                            <label>
+                                <input value="Оплата при получении в службах доставки" name='payment' type='radio'>
+                                Оплата при получении в службах доставки
+                            </label>
+                            <label>
+                                <input value="Оплата по безналичному расчету (НДС)" name='payment' type='radio'>
+                                Оплата по безналичному расчету (НДС)
+                            </label>
+                        </div>
+                        <button type='submit' id="submit" class='btn'>Создать Заказ</button>
+                    </form>
                 </div>
                 <div class='ordering-basket'>
                     <div class='ordering-basket-item'>
@@ -55,6 +142,6 @@
 <?php get_footer(); ?>
 
 <script>
-    const item = document.getElementsByClassName('products-js')[0];
-    item.value = JSON.stringify(products);
+  const item = document.getElementsByClassName('products-js')[0];
+  item.value = JSON.stringify(products);
 </script>
